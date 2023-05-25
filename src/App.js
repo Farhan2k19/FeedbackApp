1ba304e0b70800531fb8553ec57390fa306e8648
Farhan <farhanahmed2k177@gmail.com>
2023-05-25 21:16:30 +0500

    all

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 function App(){
- return <h1>Hello from the app component</h1>
-
+ return(
+    <div className="container">
+     <h1>Hello from the app component</h1>
+     <p>Hello</p>
+     
+     </div>
+ )
+ 
 }
 
 export default App
